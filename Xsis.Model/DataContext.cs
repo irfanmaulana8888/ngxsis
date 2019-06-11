3703491a27c3f5f50cb6887e7e65268bddc8298a
irfanmaulana88 <irfanmaulanaa8888@gmail.com>
2019-06-11 21:36:44 +0700

    Penambahan Class database baru

--- Xsis.Model/DataContext.cs
+++ Xsis.Model/DataContext.cs
@@ -23,8 +23,38 @@ namespace Xsis.Model
         public virtual DbSet<Catatan> Catatan { get; set; }
         public virtual DbSet<Company> Company { get; set; }
         public virtual DbSet<Educational_Level> Educational_Level { get; set; }
+        public virtual DbSet<Family_Relation> Family_Relation { get; set; }
+        public virtual DbSet<Family_Tree> Family_Tree { get; set; }
+        public virtual DbSet<Identity_Type> Identity_Type { get; set; }
+        public virtual DbSet<Keahlian> Keahlian { get; set; }
+        public virtual DbSet<Keluarga> Keluarga { get; set; }
+        public virtual DbSet<Keterangan_Tambahan> Keterangan_Tambahan { get; set; }
+        public virtual DbSet<Maritial_Status> Maritial_Status { get; set; }
+        public virtual DbSet<Menu_Access> Menu_Access { get; set; }
+        public virtual DbSet<MenuTree> MenuTree { get; set; }
+        public virtual DbSet<Note_Type> Note_Type { get; set; }
+        public virtual DbSet<Online_Test> Online_Test { get; set; }
+        public virtual DbSet<Online_Test_Detail> Online_Test_Detail { get; set; }
+        public virtual DbSet<Organisasi> Organisasi { get; set; }
+        public virtual DbSet<Pe_Referensi> Pe_Referensi { get; set; }
+        public virtual DbSet<Religion> Religion { get; set; }
+        public virtual DbSet<Rencana_Jadwal> Rencana_Jadwal { get; set; }
+        public virtual DbSet<Rencana_Jadwal_Detail> Rencana_Jadwal_Detail { get; set; }
+        public virtual DbSet<Riwayat_Pekerjaan> Riwayat_Pekerjaan { get; set; }
+        public virtual DbSet<Riwayat_Pelatihan> Riwayat_Pelatihan { get; set; }
+        public virtual DbSet<Riwayat_Pendidikan> Riwayat_Pendidikan { get; set; }
+        public virtual DbSet<Riwayat_Proyek> Riwayat_Proyek { get; set; }
+        public virtual DbSet<Role> Role { get; set; }
+        public virtual DbSet<Schedule_Type> Schedule_Type { get; set; }
+        public virtual DbSet<Sertifikasi> Sertifikasi { get; set; }
+        public virtual DbSet<Sumber_Loker> Sumber_Loker { get; set; }
+        public virtual DbSet<Test_Type> Test_Type { get; set; }
+        public virtual DbSet<Time_Period> Time_Period { get; set; }
+        public virtual DbSet<Undangan> Undangan { get; set; }
+        public virtual DbSet<Undangan_Detail> Undangan_Detail { get; set; }
+        public virtual DbSet<UserRole> UserRole { get; set; }
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
