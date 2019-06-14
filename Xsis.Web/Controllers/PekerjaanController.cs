ab8e2f0b9598bd3ac2c323baf0b446491678ce6e
irfanmaulana88 <irfanmaulanaa8888@gmail.com>
2019-06-14 16:23:51 +0700

    Riwayat Pekerjaan Design (belum Kelar)

--- Xsis.Web/Controllers/PekerjaanController.cs
+++ Xsis.Web/Controllers/PekerjaanController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Xsis.Repo;
 
 namespace Xsis.Web.Controllers
 {
@@ -12,6 +13,16 @@ namespace Xsis.Web.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Tampil()
+        {
+            return Json(PekerjaanRepo.GetAll(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Create()
+        {
+            return PartialView("_Create");
         }
     }
 }
