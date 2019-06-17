0b2bb25b6493f4de129073057dfeb4e0e0074421
irfanmaulana88 <irfanmaulanaa8888@gmail.com>
2019-06-17 15:46:28 +0700

    Pengguna Revisi

--- Xsis.Model/AddrBook.cs
+++ Xsis.Model/AddrBook.cs
@@ -13,36 +13,41 @@ namespace Xsis.Model
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public long addrbook_id { get; set; }
+        public long id { get; set; }
 
+        [Required(AllowEmptyStrings = false)]
         public long created_by { get; set; }
 
         [Column(TypeName = "Date")]
+        [Required(AllowEmptyStrings = false)]
         public DateTime created_on { get; set; }
 
-        public long modified_by { get; set; }
+        public Nullable<long> modified_by { get; set; }
 
         [Column(TypeName = "Date")]
-        public DateTime modified_on { get; set; }
+        public Nullable<DateTime> modified_on { get; set; }
 
-        public long deleted_by { get; set; }
+        public Nullable<long> deleted_by { get; set; }
 
         [Column(TypeName = "Date")]
-        public DateTime deleted_on { get; set; }
+        public Nullable<DateTime> deleted_on { get; set; }
 
         public Boolean is_delete { get; set; }
 
         public Boolean is_locked { get; set; }
 
         [Column(TypeName = "Varchar")]
+        [Required(AllowEmptyStrings = false)]
         [StringLength(100)]
         public string email { get; set; }
 
         [Column(TypeName = "Varchar")]
+        [Required(AllowEmptyStrings = false)]
         [StringLength(50)]
         public string abuid { get; set; }
 
         [Column(TypeName = "Varchar")]
+        [Required(AllowEmptyStrings = false)]
         [StringLength(50)]
         public string abpwd { get; set; }
     }
