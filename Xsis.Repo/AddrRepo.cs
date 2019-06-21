c73dc7832e2b70529744311a27a3cbf8a0237910
irfanmaulana88 <irfanmaulanaa8888@gmail.com>
2019-06-21 15:52:00 +0700

    validasi Pengguna

--- Xsis.Repo/AddrRepo.cs
+++ Xsis.Repo/AddrRepo.cs
@@ -9,13 +9,51 @@ namespace Xsis.Repo
 {
     public class AddrRepo
     {
-        public static AddrBook GetByID(string ID)
+        public static List<AddrBook> GetAll()
+        {
+            List<AddrBook> result = new List<AddrBook>();
+            using (var db = new DataContext())
+            {
+
+                result = (from t in db.AddrBook
+                          where t.is_delete == false
+                          select t).ToList();
+      
+                return result;
+            }
+        }
+
+        public static AddrBook GetByEmail(string Email)
         {
             AddrBook addr = new AddrBook();
             using (DataContext db = new DataContext())
             {
-                addr = db.AddrBook.Where(d => d.email == ID).First();
+                addr = db.AddrBook.Where(d => d.email == Email && d.is_delete == false).First();
                 return addr;
+            }
+        }
+
+        public static Boolean Deleteaddr(int ID, AddrBook addrmdl)
+        {
+            try
+            {
+
+                AddrBook dep;
+                using (DataContext db = new DataContext())
+                {
+                    dep = db.AddrBook.Where(d => d.id == ID).First();
+                    dep.is_delete = true;
+                    dep.deleted_by = addrmdl.deleted_by;
+                    dep.deleted_on = DateTime.Now;
+                    db.Entry(dep).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
             }
         }
     }
--- Xsis.Web/Controllers/AddrController.cs
+++ Xsis.Web/Controllers/AddrController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Xsis.Model;
 using Xsis.Repo;
 
 namespace Xsis.Web.Controllers
@@ -15,9 +16,32 @@ namespace Xsis.Web.Controllers
             return View();
         }
 
-        public ActionResult AmbilData(string ID)
+        public ActionResult Tampil()
         {
-            return Json(AddrRepo.GetByID(ID), JsonRequestBehavior.AllowGet);
+            return Json(AddrRepo.GetAll(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult AmbilData(string Email)
+        {
+            return Json(AddrRepo.GetByEmail(Email), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult DeleteCfr(int ID)
+        {
+            return PartialView("_Delete");
+        }
+
+        public ActionResult Delete(int ID, AddrBook addrmdl)
+        {
+            addrmdl.deleted_by = Convert.ToInt64(Session["foo"]);
+            if (AddrRepo.Deleteaddr(ID, addrmdl))
+            {
+                return Json(new { Hapus = "Berhasil" }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new { Hapus = "Gagal" }, JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
